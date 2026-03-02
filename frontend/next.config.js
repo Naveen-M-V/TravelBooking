84e2c2a3a3a901ad6cc6d2fb58039d0b521877f7
Naveen MV <mvnaveen18@gmail.com>
2026-03-02 18:46:39 +0530

    SErver Config

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -7,6 +7,14 @@ const nextConfig = {
       { protocol: 'http',  hostname: 'localhost' },
     ],
   },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://localhost:5000/api/:path*',
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
